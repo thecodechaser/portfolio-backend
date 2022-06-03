4da6d8633219b9a84a574dfda3479df9e3016428
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-06-03 19:52:50 +0530

    Update application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,21 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  # without api calls
+  protect_from_forgery with: :exception
+
+  # for api calls
+  # protect_from_forgery prepend: true
+
+  # prevent users to perfrom action without authentication
+  # before_action :authenticate_user!, unless: :api_path
+
+  before_action :update_allowed_parameters, if: :devise_controller?
+
+  protected
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :photo_link, :bio, :portfolio_link, :email, :password) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
+  end
 end
