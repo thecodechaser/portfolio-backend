41724451fd5a263f34698a37211486570c6a1d07
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-06-04 18:23:45 +0530

    Add funbction for comment counter

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,4 +4,12 @@ class Comment < ApplicationRecord
   validates :comment, presence: true
   validates :author, presence: true
   validates :avatar, presence: true
+
+  after_save :update_comments_counter
+
+  private
+
+  def update_comments_counter
+    post.increment!(:comments_counter)
+  end
 end
