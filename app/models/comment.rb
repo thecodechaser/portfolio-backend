1704fc5edf7ba0b894fa063caf0fb91d89c1f2bc
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-06-04 18:21:08 +0530

    Add validation in comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,14 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :post
 
-  validates :title, presence: true
-  validates :h_one, presence: true
-  validates :p_one, presence: true
-  validates :h_two, presence: true
-  validates :p_two, presence: true
-  validates :h_three, presence: true
-  validates :p_three, presence: true
-  validates :conclusion, presence: true
-  validates :photo_one, presence: true
-  validates :photo_two, presence: true
+  validates :comment, presence: true
+  validates :author, presence: true
+  validates :avatar, presence: true
 end
