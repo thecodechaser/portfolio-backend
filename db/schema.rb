7765fba3d6f62319fc7f4281d3a5bcbe0ba8d49a
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-06-04 17:49:42 +0530

    Generate posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_03_141022) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_04_121157) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "title"
+    t.string "h_one"
+    t.text "p_one"
+    t.string "h_two"
+    t.text "p_two"
+    t.string "h_three"
+    t.text "p_three"
+    t.text "conclusion"
+    t.string "photo_one"
+    t.string "photo_two"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -30,4 +47,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_03_141022) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
