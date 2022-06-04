e8164ec7a9f9c5457d55ac411b9c5bae7a79970a
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-06-04 20:08:42 +0530

    Add register endpoint

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,3 +1,10 @@
 class Api::V1::DoctorsController < ApplicationController
-  
+  def register
+    new_user = User.new(name: params[:name], email: params[:email], password: params[:password])
+    if new_user.save
+      render json: { success: true, message: 'User created', data: { user: new_user } }, status: :created
+    else
+      render json: { success: false, errors: new_user.errors }, status: :unprocessable_entity
+    end
+  end
 end
