e9078ce5ed661617595b440a660e6fc673089448
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-06-04 17:55:44 +0530

    Add valdations in post controller

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,15 @@
 class Post < ApplicationRecord
 
   belongs_to :user
+
+  validates :title, presence: true
+  validates :h_one, presence: true
+  validates :p_one, presence: true
+  validates :h_two, presence: true
+  validates :p_two, presence: true
+  validates :h_three, presence: true
+  validates :p_three, presence: true
+  validates :conclusion, presence: true
+  validates :photo_one, presence: true
+  validates :photo_two, presence: true
 end
