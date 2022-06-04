c593c1e93f94a98dfe6203c142cffc7f02f619c9
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-06-04 18:05:30 +0530

    Create tablw of comments in schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_04_121157) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_04_123159) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.bigint "post_id", null: false
+    t.text "comment"
+    t.string "author"
+    t.string "avatar"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -47,5 +57,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_04_121157) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "posts"
   add_foreign_key "posts", "users"
 end
