338acfb11d4af301a2059e007cfa6953d51b8d1f
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-06-06 13:09:57 +0530

    Fix issues

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,21 +1,22 @@
-class API::V1::PostsController <  ApplicationController
+# frozen_string_literal: true
 
+class API::V1::PostsController < ApplicationController
   def index
     if request.headers['X-AUTH-TOKEN']
       user = User.find_by_api_token(request.headers['X-AUTH-TOKEN'])
       if user
-    posts = Post.all 
-    if posts
-      render json: { success: true, message: 'Posts loaded', data: { user: posts } }, status: :created
+        posts = Post.all
+        if posts
+          render json: { success: true, message: 'Posts loaded', data: { user: posts } }, status: :created
+        else
+          render json: { success: false, errors: posts.errors }, status: :unprocessable_entity
+        end
+      else
+        render json: { success: false, errors: 'Wrong authentication token' }, status: :unprocessable_entity
+      end
     else
-      render json: { success: false, errors: posts.errors }, status: :unprocessable_entity
+      render json: { success: false, message: 'please sign in or add the token' }, status: :ok
     end
-  else
-    render json: { success: false, errors: 'Wrong authentication token' }, status: :unprocessable_entity
-  end
-  else
-    render json: { success: false, message: 'please sign in or add the token' }, status: :ok
-end
   end
 
   def create
@@ -23,8 +24,8 @@ end
       user = User.find_by_api_token(request.headers['X-AUTH-TOKEN'])
       if user
         new_post = user.posts.new(title: params[:title], h_one: params[:h_one], p_one: params[:p_one], h_two: params[:h_two], p_two: params[:p_two],
-                   h_three: params[:h_three], p_three: params[:p_three], conclusion: params[:conclusion], photo_one: params[:photo_one],
-                  photo_two: params[:photo_two])
+                                  h_three: params[:h_three], p_three: params[:p_three], conclusion: params[:conclusion], photo_one: params[:photo_one],
+                                  photo_two: params[:photo_two])
         if new_post.save
           render json: { success: true, message: 'Post created', data: { post: new_post } }, status: :created
         else
@@ -34,17 +35,25 @@ end
         render json: { success: false, errors: 'Wrong authentication token' }, status: :unprocessable_entity
       end
     else
-        render json: { success: false, message: 'please sign in or add the token' }, status: :ok
+      render json: { success: false, message: 'please sign in or add the token' }, status: :ok
     end
   end
 
-
   def destory
+    if request.headers['X-AUTH-TOKEN']
+      user = User.find_by_api_token(request.headers['X-AUTH-TOKEN'])
+      if user
     post = Post.find(params[:id])
     if post.destory
-      render json: { success: true, message: 'Post deleted', data: { post: post } }, status: :deleted
-        else
-          render json: { success: false, errors: new_post.errors }, status: :unprocessable_entity
+      render json: { success: true, message: 'Post deleted', data: { post: } }, status: :deleted
+    else
+      render json: { success: false, errors: new_post.errors }, status: :unprocessable_entity
+    end
+  else
+    render json: { success: false, errors: 'Wrong authentication token' }, status: :unprocessable_entity
   end
-
+else
+  render json: { success: false, message: 'please sign in or add the token' }, status: :ok
+end
+  end
 end
