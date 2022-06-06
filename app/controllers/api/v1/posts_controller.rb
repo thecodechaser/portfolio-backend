785b7df7c683a0ba14293959e1a693e637589cfa
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-06-06 12:53:25 +0530

    Complete index function

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,7 +1,12 @@
 class API::V1::PostsController <  ApplicationController
 
   def index
-    posts 
+    posts = Post.all 
+    if posts
+      render json: { success: true, message: 'Posts loaded', data: { user: posts } }, status: :created
+    else
+      render json: { success: false, errors: posts.errors }, status: :unprocessable_entity
+    end
   end
 
   def create
