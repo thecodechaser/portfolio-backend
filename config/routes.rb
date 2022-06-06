0ce705f080309eda917b4b7ad69095548710a58d
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-06-06 12:46:36 +0530

    Create routes for posts

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,9 @@ Rails.application.routes.draw do
     namespace :v1 do
       post 'users/sign_up' => 'users#register'
       post 'users/sign_in' => 'users#login'
+      get 'posts/load' => 'posts#index'
+      post 'posts/create' => 'posts#create'
+      delete 'posts/delete' => 'posts#destory'
     end
   end   
 
