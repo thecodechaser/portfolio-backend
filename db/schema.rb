c03ff0be727722e33493dc0f97f9fd47d91294d8
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-06-08 10:54:20 +0530

    Create file reply.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_04_144607) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_08_052141) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,6 +50,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_04_144607) do
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
+  create_table "replies", force: :cascade do |t|
+    t.bigint "comment_id", null: false
+    t.text "reply"
+    t.string "author"
+    t.string "avatar"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["comment_id"], name: "index_replies_on_comment_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "photo_link"
@@ -70,4 +80,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_04_144607) do
   add_foreign_key "comments", "posts"
   add_foreign_key "likes", "posts"
   add_foreign_key "posts", "users"
+  add_foreign_key "replies", "comments"
 end
